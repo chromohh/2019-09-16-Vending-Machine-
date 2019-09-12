36855f25f579413ff6415df00de2e739b6c20a3f
chromohh <sindre.he@hotmail.com>
2019-09-12 14:12:26 +0200

    enum

--- src/main/java/sindre/VendingMachine/ImplementVM.java
+++ src/main/java/sindre/VendingMachine/ImplementVM.java
@@ -1,6 +1,6 @@
 package sindre.VendingMachine;
 
-import sindre.Product.Products;
+import sindre.Product.*;
 
 public class ImplementVM implements VendingMachine{
     public enum Money {
@@ -12,19 +12,39 @@ public class ImplementVM implements VendingMachine{
         Hundra(100),
         Femhundra(500),
         Tusen(1000);
-        private final int value;
+        final int value;
         Money(int value) {
             this.value = value;
         }
+        public int getValue(){return value;}
     }
 
-    public ImplementVM(){}
+    private int moneyPool;
+    private Products[] productArr;
+
+    public ImplementVM(){
+        this.moneyPool = 0;
+        this.productArr = new Products[5];
+        productArr[0] = new Candy("Twix", "helt vanlig twix", 143, 13, 43);
+        productArr[1] = new Drinks("Coca Cola", "helt vanliga cola", 99, 15, 44);
+        productArr[2] = new Food("Stroganoff", "helt vanlig stroganoff", 253, 40, 45);
+        productArr[3] = new Fruit("Banan", "helt vanlig banan", 100, 8, 46);
+        productArr[4] = new Candy("Snickers", "helt vanlig snickers", 190, 13, 47);
+    }
 
     @Override
-    public void addCurrency(int amount) {
-
+    public void addCurrency(int money) {
+        boolean wasFound = false;
+        for (Money x : Money.values()) {
+            if (money == x.getValue()) {
+                moneyPool = moneyPool + money;
+                wasFound = true;
+            }
+        }
+        if (!wasFound) {
+            throw new IllegalArgumentException("Wrong input, only 1,5,10,20,50,100,500,1000");
+        }
     }
-
     @Override
     public Products request(int productNumber) {
         return null;
